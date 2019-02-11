45cf13b638ec7d4044cb61898d42718794fcee20
Tushar D <tdongare@cemtrexlabs.com>
2019-02-11 19:46:11 +0530

    router added

--- client/App.js
+++ client/App.js
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
-import MainContainer from './Containers/MainContainer';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Layout from './Components/Layout/Layout';
 import './App.scss';
 
-export default class App extends Component {
+
+// wrapping/composing
+const BasicRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )} />
+);
+
+const Home = function () {
+  return <div>Home <Link to="/login">LLL</Link></div>;
+}
+
+const Login = function () {
+  return <div>Login</div>;
+}
+
+
+export default class App extends React.Component {
 
   render() {
     return (
       <div className="App">
-        <MainContainer />
+        <Router>
+          <React.Fragment>
+            <BasicRoute exact path="/" component={Home} />
+            <BasicRoute path="/login" component={Login} />
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
